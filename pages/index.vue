04596a05e4b5644b27f0838207b5b36120fdad92
SuzukiFumito <Fumito@MacBook-Pro-2.local>
2020-10-27 09:00:22 +0900

    inspection env

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,24 @@
     <FirstView />
     <Introduction />
     <Practicable />
+    <b-container>
+      <b-row>
+        <b-col>
+          <p>{{ env }}</p>
+        </b-col>
+      </b-row>
+    </b-container>
     <Information />
     <TheNavbar />
   </article>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      env: process.env.NODE_ENV,
+    }
+  },
+}
+</script>
