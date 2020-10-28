2748bea00fe92c178970ee1ecb7313d2bf923281
SuzukiFumito <Fumito@MacBook-Pro-2.local>
2020-10-29 02:28:18 +0900

    Add contentful host env.

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -4,6 +4,7 @@ const contentful = require('contentful')
 const config = {
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
+  host: process.env.CTF_HOST,
 }
 
 // export `createClient` to use it in page components
