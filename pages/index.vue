95f0d989b89e184362a6d1a153a5c2c834bf0de4
SuzukiFumito <Fumito@MacBook-Pro-2.local>
2020-10-29 02:00:57 +0900

    検証用の出力を削除

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,7 @@
     <FirstView />
     <Introduction />
     <Practicable />
-    <b-container>
-      <b-row>
-        <b-col>
-          <p>{{ env }}</p>
-        </b-col>
-      </b-row>
-    </b-container>
     <Information />
     <TheNavbar />
   </article>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      env: process.env.NETLIFY,
-    }
-  },
-}
-</script>
